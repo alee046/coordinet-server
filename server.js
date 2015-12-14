e2e9c5ab85e721fa6a6fea9e18bfc03fb1e392f6
alee <alee046@ucr.edu>
2015-12-14 15:46:38 -0800

    initial server

---
+++ server.js
@@ -0,0 +1,23 @@
+
+//
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(process.env.PORT || 8000);
+
+io.sockets.on('connection', function (socket) {
+  console.log('new user connected!');
+  socket.on('location', function (data) {
+    io.sockets.emit('location', data);
+  });
+});
+
+// For serving static files inside ./client
+app.use(require('express').static(__dirname + '/client'));
+
+// For hosing on Heroku
+// io.configure(function () {
+//   io.set("transports", ["xhr-polling"]);
+//   io.set("polling duration", 10);
+//   io.set('log level', 1)
+// });
